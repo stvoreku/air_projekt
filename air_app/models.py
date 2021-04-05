fc4aca920703b4d311a173f694a6bd4be323bef6
Jacek <stvoreku@gmail.com>
2021-04-05 20:11:13 +0200

    Podpiecie url nowej apki

--- air_app/models.py
+++ air_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    #TODO optimalization - consider long int vs float
+    x = models.FloatField()
+    y = models.FloatField()
