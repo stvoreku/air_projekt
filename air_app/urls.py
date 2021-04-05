b5eda46e56b700fe725600cdd84158ee12292f1d
Jacek <stvoreku@gmail.com>
2021-04-05 18:22:39 +0200

    Podpiecie url nowej apki

---
+++ air_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+from django.views.decorators.csrf import csrf_exempt
+urlpatterns = [
+    path('', csrf_exempt(views.HomeView.as_view()), name = 'home'),
+]
