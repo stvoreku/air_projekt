d57028221202d2b504a7c3becb7f92a9f65e2fff
Jacek <stvoreku@gmail.com>
2021-05-27 09:26:37 +0200

    vue full

--- air_front/src/main.js
+++ air_front/src/main.js
@@ -5,7 +5,7 @@ import './assets/base.css';
 Vue.config.productionTip = false
 
 new Vue({
-  data: {'PlaceID':0, 'QueueID': 0, 'plotX': [], 'plotY': []},
+  data: {'PlaceID':0, 'QueueID': 0, 'plotX': [0,1,2,4,3,2], 'plotY': [1,1,2,3,3,1]},
   render: h => h(App),
 }).$mount('#app')
 
